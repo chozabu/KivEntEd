99ec78831cd154518e9dcb20766a9ce1f053ac98
Chozabu <chozabu@gmail.com>
2014-06-02 19:17:28 +0100

    new script, push in object direction

---
+++ scripts/collision/pushSecondWithFirst.py
@@ -0,0 +1,30 @@
+__author__ = 'chozabu'
+
+import cymunk as cy
+from math import *
+
+#this is a refrence to kiventeds scripting system
+scripty = None
+
+defaults  = {
+	"pushforce": 30,
+}
+
+def collision_func(space, arbiter):
+	first_body = arbiter.shapes[0].body
+	second_body = arbiter.shapes[1].body
+
+
+	e1 = scripty.gameref.getEntFromID(first_body.data)
+	if hasattr(e1, 'datadict'):
+		dd = e1.datadict
+		impulse = cy.Vec2d(cos(first_body.angle),sin(first_body.angle))
+		forcemul = 0
+		if 'forcemul' in dd:
+			forcemul=float(dd['pushforce'])
+		else:
+			forcemul=float(defaults['pushforce'])
+		impulse.x*=forcemul
+		impulse.y*=forcemul
+		second_body.apply_impulse(impulse)
+	return False
