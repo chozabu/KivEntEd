5334f2c7a28b3acfea61900c19b5cadfbc081a66
Chozabu <chozabu@gmail.com>
2014-06-17 09:46:42 +0100

    prevent double loading of level on resume

--- main.py
+++ main.py
@@ -600,6 +600,7 @@ class KivEntEd(App):
 		self.root.serials.exportJSON(fileName="pauselevel.json")
 		return True
 	def on_resume(self):
+		self.root.clearAll()
 		self.root.serials.loadJSON(fileName="pauselevel.json")
 
 
