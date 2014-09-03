d4e8eb31da6e0862e84e796a8e20ff19fc02d2d5
Chozabu <chozabu@gmail.com>
2014-09-03 12:36:53 +0100

    fix sounds playing

--- scripts/collision/playSound.py
+++ scripts/collision/playSound.py
@@ -1,10 +1,11 @@
 __author__ = 'chozabu'
 import cymunk as cy
 
-from kivy.core.audio import SoundLoader
+from kivy.core.audio import SoundLoader, Sound
 
 sound = SoundLoader.load('sounds/bing.wav')
 
+#Sound().status()
 def collision_func(space, arbiter):
 	if arbiter.is_first_contact == 1:
 		second_body = arbiter.shapes[1].body
@@ -12,6 +13,6 @@ def collision_func(space, arbiter):
 		s2 = (bv.x*bv.x+bv.y*bv.y)
 		#print s2
 		if s2>1000:
-			sound.stop()
+			if sound.status == 'play': sound.stop()
 			sound.play()
 	return True
